42c4f90e0289f25a2e64741b398a2c83063805c4
mapserver2007 <mapserver2007@gmail.com>
2015-11-02 14:07:26 +0900

    設定書き換えシェルスクリプト修正

--- init.sh
+++ init.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
-  echo "Required two args" 1>&2
-  exit 1
+# usage:
+# $> sudo sh init.sh https://github.com/rankforce/infrastructure.git master ruby2.2
+
+# ansible task
+taskfile="ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml"
+
+# get project name
+git_uri="$1"
+filename=${git_uri##*/}
+project=${filename%.*}
+
+# get gemfile path
+gemfile=""
+if [ -n "$4" ]; then
+  gemfile=$4
 fi
 
 # replace text
-script1="sed -i -e 's/%REPOSITORY_URL%/$1/g' ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml"
-script2="sed -i -e 's/%BRANCH%/$2/g' ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml"
-
-eval ${script1}
-eval ${script2}
+cmd1="sed -i -e 's@%REPOSITORY%@$1@g' $taskfile"
+cmd2="sed -i -e 's@%BRANCH%@$2@g' $taskfile"
+cmd3="sed -i -e 's@%PROJECT%@$project@g' $taskfile"
+cmd4="sed -i -e 's@%GEMFILE%@gemfile@g' $taskfile"
+eval ${cmd1}
+eval ${cmd2}
+eval ${cmd3}
+eval ${cmd4}
 
 # execte script
 script="ansible-playbook -t $3 -i 'localhost,' ansible/setup.yml"
