d5219d8a0044f02e8827892cbf24452ad37e7845
mapserver2007 <mapserver2007@gmail.com>
2015-11-02 11:41:49 +0900

    コマンド修正

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ fi
 
 # replace text
 sed -i -e "s/%REPOSITORY_URL%/$1/g" ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml
-sed -i -e "s/%BRANCH%/master/g" ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml
+sed -i -e "s/%BRANCH%/$2/g" ansible/roles/applications/ruby/files/ansible/roles/settings/tasks/main.yml
 
 # execte script
-script="ansible-playbook -t $2 -i 'localhost,' ansible/setup.yml"
+script="ansible-playbook -t $3 -i 'localhost,' ansible/setup.yml"
 ${script}
