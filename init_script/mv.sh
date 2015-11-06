be25ad070651616b1cb35a2522d1321329ba305c
mapserver2007 <mapserver2007@gmail.com>
2015-11-06 23:20:31 +0900

    パス間違ってたので修正

--- init_script/mv.sh
+++ init_script/mv.sh
@@ -3,11 +3,9 @@
 # $> sudo sh mv.sh config.yml sample/php/config/
 
 # ansible task file
-upload_dir="ansible/roles/applications/php/files/upload/$2"
-mkdir="mkdir -p $upload_dir/upload"
-mv="mv $1 $upload_dir/upload/$1"
-
-# taskfile2="ansible/roles/applications/tasks/main.yml"
+upload_dir="ansible/roles/applications/php/files/upload/upload/$2"
+mkdir="mkdir -p $upload_dir"
+mv="mv $1 $upload_dir/$1"
 
 eval ${mkdir}
 eval ${mv}
