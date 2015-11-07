6b7e9a3741f5ba0d0ccbd046150a51e2c0e54abb
mapserver2007 <mapserver2007@gmail.com>
2015-11-07 11:11:35 +0900

    パス修正

--- sample2/ruby/lib/sample.rb
+++ sample2/ruby/lib/sample.rb
@@ -6,7 +6,7 @@ class Sample
   end
 
   def load_secret
-    config = YAML.load_file("../config/secret.yml")
+    config = YAML.load_file(File.dirname(__FILE__) + "/../config/secret.yml")
     config["key"]
   end
 end
