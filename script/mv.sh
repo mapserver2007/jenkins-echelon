1484cf1a29cc043c54c583a0838ec6f336409e77
mapserver2007 <mapserver2007@gmail.com>
2015-11-08 23:34:38 +0900

    シェルスクリプト修正

--- script/mv.sh
+++ script/mv.sh
@@ -7,7 +7,7 @@ langs=("php" "ruby")
 for lang in ${langs[@]}; do
   upload_dir="ansible/roles/applications/$lang/files/upload/upload/$2"
   mkdir="mkdir -p $upload_dir"
-  mv="cp -f $1 ${upload_dir}$1"
+  mv="cp -f $1 ${upload_dir}/$1"
 
   eval ${mkdir}
   eval ${mv}
