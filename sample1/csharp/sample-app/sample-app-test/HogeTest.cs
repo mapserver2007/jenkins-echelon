d4661724830845eca5fb03f4dcc38ccaa0e685e3
mapserver2007 <mapserver2007@gmail.com>
2015-11-14 12:18:06 +0900

    xUnit.netのサンプル追加

---
+++ sample1/csharp/sample-app/sample-app-test/HogeTest.cs
@@ -0,0 +1,15 @@
+﻿using SampleApp;
+using Xunit;
+
+namespace sample_app_test
+{
+    public class HogeTest
+    {
+        [Fact]    
+        public void TestName()
+        {
+            var hoge = new Hoge();
+            Assert.Equal(hoge.GetName(), "hoge");
+        }
+    }
+}
---
+++ sample1/csharp/sample-app/sample-app/Hoge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SampleApp
+{
+    public class Hoge
+    {
+        public int MyProperty { get; set; }
+
+        public string GetName()
+        {
+            return "hoge";
+        }
+    }
+
+}
